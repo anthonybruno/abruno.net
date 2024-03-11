795d6f609625f9eca94ea246878c5113f5aab623
Anthony Bruno <anthonybruno@gmail.com>
2024-03-11 15:43:41 -0600

    Tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import { sanityIntegration } from "@sanity/astro";
+
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://abruno.net",
+  prefetch: true,
+  integrations: [
+    // sanityIntegration({
+    //   projectId: "",
+    //   dataset: "production",
+    //   useCdn: false,
+    // }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    // partytown({
+    //   config: {
+    //     forward: ["dataLayer.push"],
+    //   },
+    // }),
+  ],
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "system-ui", "sans-serif"],
+    },
+    extend: {
+    },
+  },
+  plugins: [
+    // require("@tailwindcss/typography")
+  ],
+};
