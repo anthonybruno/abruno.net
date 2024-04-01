79de2ab7838213b4aa8ba3a7bf4f62b64243515d
Anthony Bruno <anthonybruno@gmail.com>
2024-04-01 10:09:43 -0600

    Adding new deps for @sanity/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,32 @@
 import { defineConfig } from "astro/config";
-import { sanityIntegration } from "@sanity/astro";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://abruno.net",
   prefetch: true,
   integrations: [
-    sanityIntegration({
+    sanity({
       projectId: "vqft6e9t",
       dataset: "production",
       useCdn: false
-    }), 
+    }),
     tailwind({
-      applyBaseStyles: false
-    }),   
+    applyBaseStyles: false
+    }), 
     partytown({
       config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
+        forward: ["dataLayer.push"]
+      }
+    }), 
     sitemap({
-      filter: (page) =>
-        page !== 'https://abruno.net/copy-mailto-privacy/'
-    }),
+      filter: page => page !== 'https://abruno.net/copy-mailto-privacy/'
+    }), 
     alpinejs()
   ]
 });
