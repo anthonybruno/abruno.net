71292834abf73d95a56014c5c22282dc49a320a5
Anthony Bruno <anthonybruno@gmail.com>
2024-07-17 19:43:04 -0600

    Remove currently

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-import alpinejs from "@astrojs/alpinejs";
 
 import sanity from "@sanity/astro";
 
@@ -27,6 +26,5 @@ export default defineConfig({
     sitemap({
       filter: page => page !== 'https://abruno.net/copy-mailto-privacy/'
     }), 
-    alpinejs()
   ]
 });
