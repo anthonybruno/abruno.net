fee07444cbdeecf4943bf6da9ba2ae55535b426a
Anthony Bruno <anthonybruno@gmail.com>
2025-09-18 18:46:24 -0600

    fix: build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@preact/preset-vite';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), preact()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
 });
